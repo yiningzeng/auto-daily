cc67d547b2b272f49dbe3b96e2db26ec18290d79
Unknown <unknown@example.com>
2019-07-22 00:43:48 +0800

    feat: 修复日报格式

--- auto_create_daily.sh
+++ auto_create_daily.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 date=$(date "+%Y-%m-%d")
 mkdir $date
-echo "# "$date" 日报(曾伟)" >> $date"/007.txt"
-echo "---" >> $date"/007.txt"
-echo "## 1." >> $date"/007.txt"
-echo "## 2." >> $date"/007.txt"
-echo "## 3." >> $date"/007.txt"
-git add $date"/007.txt"
+echo "# "$date" 日报(曾伟)" >> $date"/007.md"
+echo "---" >> $date"/007.md"
+echo "## 1." >> $date"/007.md"
+echo "## 2." >> $date"/007.md"
+echo "## 3." >> $date"/007.md"
+git add $date"/007.md"
 git commit -m "feat: "$date"日常添加日报文件"
 git push -u origin master
