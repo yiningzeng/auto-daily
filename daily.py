fabfed09be8a9ff27ec618c32dfcfbf2f42b4040
yiningzeng <yiningzeng@outlook.com>
2019-07-25 08:37:33 +0800

    feat: 自动push日志

--- daily.py
+++ daily.py
@@ -26,14 +26,20 @@ if __name__ == '__main__':
             pre = msg.member.name + " " + msg.member.display_name + ' @提醒: '
             msg.forward(bot.file_helper, prefix=pre)
             if is_reply == "1":
-                return "已提醒: 收到👌"
+                return "收到👌"
         else:
             os.system("notify-send '%s - %s' '%s' -t %d" % (msg.sender, msg.create_time, msg, 10000))
 
     def create_daily():
         os.system('sh auto_create_daily.sh')
         # os.system('sh test.sh')
-        print('Job1-create_daily:%s' % (datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')))
+        print('Job-create_daily:%s' % (datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')))
+
+    def push_daily():
+        os.system('sh auto_push_daily.sh')
+        bot.file_helper.send("已经push日志，如果已经填写，请忽略。20:30分准时发日报")
+        # os.system('sh test.sh')
+        print('Job-push_daily:%s' % (datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')))
         """
         echo "# -" >> README.md
         git init
@@ -65,10 +71,12 @@ if __name__ == '__main__':
     # 创建后台执行的 schedulers
     scheduler = BackgroundScheduler()
     # 添加调度任务
-    # 调度方法为 timedTask，触发器选择 interval(间隔性)，间隔时长为 2 秒  0 15 10 ? * MON-FRI
-    scheduler.add_job(send_daily, 'cron', second="0", minute="0", hour="18", day_of_week="MON-SAT")
-
     scheduler.add_job(create_daily, 'cron', second="0", minute="5", hour="1", day_of_week="MON-SUN")
+    scheduler.add_job(push_daily, 'cron', second="0", minute="5", hour="19", day_of_week="MON-SUN")
+    # 调度方法为 timedTask，触发器选择 interval(间隔性)，间隔时长为 2 秒  0 15 10 ? * MON-FRI
+    scheduler.add_job(send_daily, 'cron', second="0", minute="30", hour="20", day_of_week="MON-SAT")
+
+
     # 启动调度任务
     scheduler.start()
     bot.file_helper.send('runing:%s' % (datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')))
